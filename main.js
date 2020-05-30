a415da0c329f4b15d33a69cc326df78961e752d1
Wojciech Łukowski <wojtek304@gmail.com>
2020-05-30 22:52:41 +0200

    Create toggleHandler function

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+const prices = document.querySelectorAll('.price');
+const toggle = document.querySelector('.toggle__checkbox');
+
 const pricing = {
 	monthly: {
-		basic: '&dollar;19.99',
-		professional: '&dollar;24.99',
-		master: '&dollar;39.99',
+		basic: '19.99',
+		professional: '24.99',
+		master: '39.99',
 	},
 	anually: {
-		basic: '&dollar;199.99',
-		professional: '&dollar;249.99',
-		master: '&dollar;399.99',
+		basic: '199.99',
+		professional: '249.99',
+		master: '399.99',
 	},
 };
+
+const toggleHandler = function () {
+	if (this.checked) {
+		this.setAttribute('aria-checked', 'true');
+		prices.forEach((price) => {
+			const pricingPlan = price.dataset.pricingPlan;
+			price.textContent = pricing.monthly[pricingPlan];
+		});
+	} else {
+		this.setAttribute('aria-checked', 'false');
+		prices.forEach((price) => {
+			const pricingPlan = price.dataset.pricingPlan;
+			price.textContent = pricing.anually[pricingPlan];
+		});
+	}
+};
+
+toggle.addEventListener('click', toggleHandler);
