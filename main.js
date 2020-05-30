782202208398c8279660d9166bde592699fff290
Wojciech Łukowski <wojtek304@gmail.com>
2020-05-30 16:30:05 +0200

    Initial commit with HTML base, base styles for HTML, css-variables and pricing object in JS

---
+++ main.js
@@ -0,0 +1,12 @@
+const pricing = {
+	monthly: {
+		basic: '&dollar;19.99',
+		professional: '&dollar;24.99',
+		master: '&dollar;39.99',
+	},
+	anually: {
+		basic: '&dollar;199.99',
+		professional: '&dollar;249.99',
+		master: '&dollar;399.99',
+	},
+};
